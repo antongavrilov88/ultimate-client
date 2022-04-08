ab4284264b6e463f25eade2f9a410383b3c03e76
Anton <antongavrilov88@gmail.com>
2022-04-08 20:50:46 +0200

    chore: add typescript and tsconfig

---
+++ lint-staged.config.js
@@ -0,0 +1,22 @@
+const { ESLint } = require('eslint');
+
+/**
+ * Удаление из проверки lint-staged файлов, которые находятся в .eslintignore
+ * @param {string[]}files имена файлов, находящиеся в stage гита
+ */
+const getFilesToLint = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
+  return files.filter((_, index) => !isIgnored[index]);
+};
+
+module.exports = {
+  'src/**/*.(js|jsx|ts|tsx)': async (filenames) => {
+    const filesToLint = await getFilesToLint(filenames);
+    return [
+      `eslint ${filesToLint.join(' ')} --rule "{no-console: 2, no-alert:2, no-debugger: 2}"`,
+      `prettier ${filenames.join(' ')} -write`,
+      'yarn test-changed',
+    ];
+  },
+};
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-const foo = () => console.log();
-export { foo };
